bd0d00dc3c3b89405cdd4ddb3df839b96b93a138
goodluck <goodluckkumar04@gmail.com>
2024-03-04 20:06:54 +0530

    forgot api and change password

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { userLoggedIn } from "@/redux/features/auth/authSlice";
-import Image from "next/image";
-import { useDispatch } from "react-redux";
 
 
 export default function Home() {
-  const dispatch = useDispatch()
   return (
     <div>
-      {/* <Button onClick={
-        () => dispatch(userLoggedIn({accessToken:"lsnfklnfwbgwjgwle", user:"goodluck"}))
-      }>Hello</Button> */}
     </div>
   );
 }
